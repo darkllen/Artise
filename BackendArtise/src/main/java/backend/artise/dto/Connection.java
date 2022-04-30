64db5f1b04422b09a89c6e46216a1465b4fd39e4
darkllen <yankiniigor@gmail.com>
2022-04-30 23:00:00 +0300

    add connection table

--- BackendArtise/src/main/java/backend/artise/dto/Connection.java
+++ BackendArtise/src/main/java/backend/artise/dto/Connection.java
@@ -1,8 +1,8 @@
 package backend.artise.dto;
 
+
 import backend.artise.EntityIdResolver;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,57 +12,38 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "user_service")
+@Table(name = "connection")
 @ToString
-public class UserService {
+public class Connection {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
-
-    @Column(name = "time")
-    @NotNull
-    private Date time;
-
     @Column(name = "status")
     @NotNull
     private Integer status;
 
-    @Column(name = "price")
-    @NotNull
-    private Float price;
-
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            resolver = EntityIdResolver.class,
+            property = "id",
+            scope= UserService.class)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_init_id", nullable=false)
+    private User user_init;
 
     @JsonIdentityInfo(
             generator = ObjectIdGenerators.PropertyGenerator.class,
             resolver = EntityIdResolver.class,
             property = "id",
-            scope= User.class)
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="user_id", nullable=false)
-    private User user;
-
-    @JsonIdentityInfo(
-            generator = ObjectIdGenerators.PropertyGenerator.class,
-            resolver = EntityIdResolver.class,
-            property = "id",
-            scope= Service.class)
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="service_id", nullable=false)
-    private Service service;
-
-        @JsonIgnore
-    @OneToMany(mappedBy="userService", fetch = FetchType.EAGER)
-    private List<Notes> notes;
-
+            scope= UserService.class)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_to_connect_id", nullable=false)
+    private User user_to_connect;
 }
-
 
--- BackendArtise/src/main/resources/db/migration/V1__init.sql
+++ BackendArtise/src/main/resources/db/migration/V1__init.sql
@@ -16,9 +16,14 @@ ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELE
 CREATE TABLE `artise`.`service` ( `id` INT NOT NULL AUTO_INCREMENT , `user_category_id` INT NOT NULL , `name` VARCHAR(50) NOT NULL , `description` VARCHAR(255) NULL , `price` FLOAT NOT NULL , `duration_minutes` INT NOT NULL , PRIMARY KEY (`id`));
 ALTER TABLE `service` ADD FOREIGN KEY (`user_category_id`) REFERENCES `user_category`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-CREATE TABLE `artise`.`user_service` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `service_id` INT NOT NULL , `time` DATETIME NOT NULL , `is_cancelled` BOOLEAN NOT NULL DEFAULT FALSE , `is_confirmed` BOOLEAN NOT NULL DEFAULT FALSE , `price` FLOAT NOT NULL , PRIMARY KEY (`id`));
+CREATE TABLE `artise`.`user_service` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `service_id` INT NOT NULL , `time` DATETIME NOT NULL , `status` INT NOT NULL DEFAULT '0' , `price` FLOAT NOT NULL , PRIMARY KEY (`id`));
 ALTER TABLE `user_service` ADD FOREIGN KEY (`service_id`) REFERENCES `service`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE `user_service` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 CREATE TABLE `artise`.`notes` ( `id` INT NOT NULL , `user_service_id` INT NOT NULL , `text` VARCHAR(255) NOT NULL );
-ALTER TABLE `notes` ADD FOREIGN KEY (`user_service_id`) REFERENCES `user_service`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `notes` ADD FOREIGN KEY (`user_service_id`) REFERENCES `user_service`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+CREATE TABLE `artise`.`connection` ( `id` INT NOT NULL AUTO_INCREMENT , `user_init_id` INT NOT NULL , `user_to_connect_id` INT NOT NULL , `status` INT NOT NULL DEFAULT '0' , PRIMARY KEY (`id`));
+ALTER TABLE `connection` ADD FOREIGN KEY (`user_init_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `connection` ADD FOREIGN KEY (`user_to_connect_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
